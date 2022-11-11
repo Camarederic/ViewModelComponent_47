1eab523f65745775b171fb1bae130a60bc0ded1c
Camarederic <camarederic@mail.ru>
2022-11-11 19:08:38 +0300

    Create CountDownTimer

--- app/src/main/java/com/example/viewmodelcomponent_47/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelcomponent_47/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.viewmodelcomponent_47
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        object : CountDownTimer(20000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+               textView.text = (millisUntilFinished/1000).toString()
+            }
+
+            override fun onFinish() {
+
+            }
+
+        }.start()
     }
 }
